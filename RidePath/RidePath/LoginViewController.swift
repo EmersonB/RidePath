2d2e8a764e7e7d090190301f2d0cd0c74a4241b4
Spencer Atkin <spenceratkin25@gmail.com>
2017-01-28 13:04:21 -0500

    Added firebase auth

--- RidePath/RidePath/LoginViewController.swift
+++ RidePath/RidePath/LoginViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
     @IBOutlet var userNameField: UITextField!
     @IBOutlet var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,9 +23,59 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func didTapLogin(_ sender: Any) {
+        if let email = userNameField.text {
+            if let password = passwordField.text {
+                if email != "" && password != "" {
+                    FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            if let errCode = FIRAuthErrorCode(rawValue: error!._code) {
+                                switch errCode {
+                                case .errorCodeUserDisabled:
+                                    print("user disabled")
+                                case .errorCodeWrongPassword:
+                                    print("incorrect password")
+                                case .errorCodeInvalidEmail:
+                                    print("invalid email")
+                                default:
+                                    print("login error: \(error)")
+                                }    
+                            }
+                        } else {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    })
+                }
+            }
+        }
     }
+    
     @IBAction func didTapRegister(_ sender: Any) {
+        if let email = userNameField.text {
+            if let password = passwordField.text {
+                if email != "" && password != "" {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+                        if error != nil {
+                            if let errCode = FIRAuthErrorCode(rawValue: error!._code) {
+                                switch errCode {
+                                case .errorCodeInvalidEmail:
+                                    print("invalid email")
+                                case .errorCodeEmailAlreadyInUse:
+                                    print("email in use")
+                                case .errorCodeWeakPassword:
+                                    print("weak password")
+                                default:
+                                    print("login error: \(error)")
+                                }
+                            }
+                        } else {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
