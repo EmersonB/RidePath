48e9a9c4e49f7f32afd20e6ed3123e6819e3cc5e
EmersonB <eberlik@gmail.com>
2017-01-28 11:37:10 -0500

    login screen changes

---
+++ RidePath/RidePath/RoundField.swift
@@ -0,0 +1,21 @@
+//
+//  RoundField.swift
+//  RidePath
+//
+//  Created by Emery Berlik on 1/28/17.
+//  Copyright © 2017 Berlik. All rights reserved.
+//
+
+import UIKit
+
+class RoundField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
