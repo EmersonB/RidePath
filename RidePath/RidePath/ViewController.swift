0f3acebda3be789eadafe113d56b35d3526a3be5
EmersonB <eberlik@gmail.com>
2017-01-28 10:18:21 -0500

    added project

---
+++ RidePath/RidePath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RidePath
+//
+//  Created by Emery Berlik on 1/28/17.
+//  Copyright © 2017 Berlik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
